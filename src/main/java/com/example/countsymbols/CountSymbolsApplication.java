e651e28263f087fe68c7c0c199ce9eab12de27c1
beautiful_far_away <assdddffff54@mail.ru>
2024-01-19 21:37:20 +0300

    Initial commit

---
+++ src/main/java/com/example/countsymbols/CountSymbolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.countsymbols;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountSymbolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountSymbolsApplication.class, args);
+	}
+
+}
